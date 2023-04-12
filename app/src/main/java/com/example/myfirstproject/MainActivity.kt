b17c9e41ddfc1a81b11c7bae207275935dc3a6d4
james <jamesrain1998@gmail.com>
2023-04-13 02:06:23 +0430

    button and show a thing on the bottom of the page in app. in this ex. we have toast and snack-bar button.

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    var mainLayout: ConstraintLayout? = null
+    var btToast: Button? = null
+    var btSnackbar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainLayout = findViewById(R.id.root)
+        btToast = findViewById(R.id.bt_toast)
+        btSnackbar = findViewById(R.id.bt_snackBar)
+    }
+
+    private fun listener (){
+        btToast?.setOnClickListener(){
+            Toast.makeText(this,"hello Ali",Toast.LENGTH_LONG).show()
+        }
     }
 }
