04ba5a0517ae257e8fd4dba3b2a0ee2c26e15643
james <jamesrain1998@gmail.com>
2023-04-13 13:45:39 +0430

    ok. now yoy can type any things like dictionary by AutoCompleteTextView

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -2,13 +2,26 @@ package com.example.myfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
+
+    var names = arrayOf("Ali", "Ahmad", "Asghar", "Emad", "Ehsan")
+    var etName: AutoCompleteTextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setData()
 
+    }
+
+    private fun setData() {
+        etName = findViewById(R.id.et_search)
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,names)
+        etName?.threshold = 1
+        etName?.setAdapter(adapter)
     }
 
 }
