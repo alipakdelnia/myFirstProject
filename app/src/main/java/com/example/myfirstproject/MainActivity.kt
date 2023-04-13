6ee7f3a2026c86e5a5759c1372829437fb4065f9
james <jamesrain1998@gmail.com>
2023-04-13 12:45:14 +0430

    all of veiws in android are here but we have a problem... not showing any things in app in phone . i don't know what to do????

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -8,34 +8,12 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    var mainLayout: ConstraintLayout? = null
-    var btToast: Button? = null
-    var btSnackbar: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mainLayout = findViewById(R.id.root)
-        btToast = findViewById(R.id.bt_toast)
-        btSnackbar = findViewById(R.id.bt_snackBar)
-        listener()
+
     }
 
-    private fun  listener(){
-        btToast?.setOnClickListener{
-            Toast.makeText(this,"Hello Ali...\n lets go",Toast.LENGTH_LONG).show()
-        }
-        btSnackbar?.setOnClickListener {
-            Snackbar.make(mainLayout!!, "Ali is in game", Snackbar.LENGTH_LONG)
-                .setAction("ok") {
-                    Toast.makeText(this, "Here we go", Toast.LENGTH_SHORT).show()
-                }.setActionTextColor(resources.getColor(R.color.purple_500))
-                .setDuration(5000)
-                .setBackgroundTint(resources.getColor(R.color.purple_200))
-                .show() // Don’t forget to show!
-        }
 
-
-
-        }
     }
 
