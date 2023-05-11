d1ca64f43f5cd5579432eb5b5d45fad9ec27d687
james <jamesrain1998@gmail.com>
2023-05-11 13:27:36 +0430

    Intent2: send data in activities you write a text in activity1 and app send it to activity2 by click ok button. just u need to send in activity1 and give it in Activity2.kt

--- app/src/main/java/com/example/myfirstproject/Activity2.kt
+++ app/src/main/java/com/example/myfirstproject/Activity2.kt
@@ -2,10 +2,18 @@ package com.example.myfirstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
+
+    var name: String = ""
+    var tvNamee: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+        tvNamee = findViewById(R.id.tv_name)
+        name=intent.getStringExtra("name").toString()
+        tvNamee?.setText(name)
     }
 }
--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.myfirstproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     var btOk: Button? = null
+    var etSend: EditText?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btOk = findViewById(R.id.bt_ok)
+        etSend=findViewById(R.id.et_send)
         listener()
 
     }
@@ -26,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private fun listener() {
         btOk?.setOnClickListener {
             var intent=Intent(this,Activity2::class.java)
+            intent.putExtra("name","I sent from activity 1 to here")
             startActivity(intent)
 
         }
