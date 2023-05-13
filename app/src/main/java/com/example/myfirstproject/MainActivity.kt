e6533da2706592d965d6343e5176ec9890d84a55
james <jamesrain1998@gmail.com>
2023-05-13 18:30:11 +0430

    vector: icons in SVG svg is a format that you can zoom in without comming sown the quality . 1.first you go in flaticon.com and download a SVG icon. 2.go to res -> drawable and right click and new and vector asset.

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -21,46 +21,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btContact = findViewById(R.id.bt_contact)
-        btWeb = findViewById(R.id.bt_web)
-        btMap = findViewById(R.id.bt_map)
-        btCall = findViewById(R.id.bt_call)
+
         listener()
 
     }
 
     private fun listener() {
-        btContact?.setOnClickListener{
-            var contact = Intent(Intent.ACTION_PICK)
-            contact.type=ContactsContract.Contacts.CONTENT_TYPE
-            startActivityForResult(contact,requsetCode2)
-        }
 
-        btWeb?.setOnClickListener{
-            var website = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.golbama.com/blog/types-of-rose-flowers"))
-            startActivity(website)
-        }
-
-        btMap?.setOnClickListener{
-            var map = Intent(Intent.ACTION_VIEW,Uri.parse("geo:51.1657,10.4515"))
-            startActivity(map)
-        }
-
-        btCall?.setOnClickListener{
-            var call=Intent(Intent.ACTION_DIAL, Uri.parse("tel:09370982564"))
-            startActivity(call)
-        }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==requsetCode2){
-            if (resultCode== RESULT_OK){
-                Toast.makeText(this,"Success",Toast.LENGTH_LONG).show()
-            }
-        }
-    }
+
+
 
 }
 
