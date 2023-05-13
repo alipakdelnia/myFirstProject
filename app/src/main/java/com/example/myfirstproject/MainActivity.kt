c7ce1462ef40dd77955c03dbe82fb05d7c0401e3
james <jamesrain1998@gmail.com>
2023-05-13 17:45:40 +0430

    Intent3 : cross to other apps in android we have 4 button and every of them go in one app its soooooooo beautiful god ♥

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,32 +1,54 @@
 package com.example.myfirstproject
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    var btOk: Button? = null
-    var etSend: EditText?= null
+    var btContact: Button? = null
+    var btWeb: Button? = null
+    var btMap: Button? = null
+    var btCall: Button? = null
+    var requsetCode=1
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btOk = findViewById(R.id.bt_ok)
-        etSend=findViewById(R.id.et_send)
+        btContact = findViewById(R.id.bt_contact)
+        btWeb = findViewById(R.id.bt_web)
+        btMap = findViewById(R.id.bt_map)
+        btCall = findViewById(R.id.bt_call)
         listener()
 
     }
 
     private fun listener() {
-        btOk?.setOnClickListener {
-            var intent=Intent(this,Activity2::class.java)
-            intent.putExtra("name","I sent from activity 1 to here")
-            startActivity(intent)
+        btContact?.setOnClickListener{
+            var contact = Intent(Intent.ACTION_PICK)
+            contact.type=ContactsContract.Contacts.CONTENT_TYPE
+            startActivityForResult(contact,requsetCode)
+        }
 
+        btWeb?.setOnClickListener{
+            var website = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.golbama.com/blog/types-of-rose-flowers"))
+            startActivity(website)
+        }
+
+        btMap?.setOnClickListener{
+            var map = Intent(Intent.ACTION_VIEW,Uri.parse("geo:51.1657,10.4515"))
+            startActivity(map)
+        }
+
+        btCall?.setOnClickListener{
+            var call=Intent(Intent.ACTION_DIAL, Uri.parse("tel:09370982564"))
+            startActivity(call)
         }
 
     }
