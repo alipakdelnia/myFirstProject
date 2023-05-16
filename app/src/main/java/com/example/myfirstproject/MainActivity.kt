93cd3cf2971cdf5fb0428d972a35bb3a1e5fdaa4
james <jamesrain1998@gmail.com>
2023-05-16 17:11:04 +0430

    dialog1 : popup window you can defined a dialog for update set cancelable help that any click disable just yes or no

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstproject
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -10,27 +11,34 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var btContact: Button? = null
-    var btWeb: Button? = null
-    var btMap: Button? = null
-    var btCall: Button? = null
-    var requsetCode2=1
-
+    var btAlertDialog: Button?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btAlertDialog=findViewById(R.id.bt_alertDialog)
         listener()
 
     }
 
-    private fun listener() {
-
-
+    private fun listener(){
+        btAlertDialog?.setOnClickListener{
+            alertDialog()
+        }
     }
-
+    private fun alertDialog(){
+        val alertDialog:AlertDialog.Builder= AlertDialog.Builder(this)
+        alertDialog.setTitle("به روز زسانی نرم افزار")
+            .setMessage("آیا میخواهید برنامه را به روزرسانی کنید؟")
+            .setCancelable(false)
+            .setPositiveButton("بله"){_,_ ->
+                Toast.makeText(this,"با موفقیت به روزرسانی شد",Toast.LENGTH_LONG).show()}
+            .setNegativeButton("خیر"){dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
+    }
 
 
 
