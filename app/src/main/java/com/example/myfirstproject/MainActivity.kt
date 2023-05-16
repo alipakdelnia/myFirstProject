e37a7d4422a2500ca080c766f1a3bcc6fd0d521d
james <jamesrain1998@gmail.com>
2023-05-16 19:05:25 +0430

    dialog2 : custom popup first right click on layout file then choose layout Resource file.

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.myfirstproject
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     var btAlertDialog: Button?=null
+    var btCustomDialog:Button?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btAlertDialog=findViewById(R.id.bt_alertDialog)
+        btCustomDialog=findViewById(R.id.bt_customDialog)
         listener()
 
     }
@@ -25,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     private fun listener(){
         btAlertDialog?.setOnClickListener{
             alertDialog()
+        }
+        btCustomDialog?.setOnClickListener{
+            customDialog()
         }
     }
     private fun alertDialog(){
@@ -40,6 +47,17 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
+    private fun customDialog(){
+        var dialog:Dialog= Dialog(this)
+        dialog.setTitle("")
+        dialog.setContentView(R.layout.dialog_layout)
+        var tvName:TextView=dialog.findViewById(R.id.tv_name)
+        var btClose:Button=dialog.findViewById(R.id.bt_close)
+        btClose.setOnClickListener{
+            dialog.dismiss()
+        }
+        dialog.show()
+}
 
 
 }
